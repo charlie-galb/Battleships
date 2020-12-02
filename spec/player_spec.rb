01f999a8f5c1fa2ade96c1559451845bf139e5d2
charlie-galb <galbraith.charlie@gmail.com>
2020-12-02 16:45:19 +0000

    Add expectations for placement of second ship

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,10 +10,16 @@ describe Player do
             it "can place ships on the board" do
                 player = Player.new([carrier, destroyer])
                 allow(carrier).to receive(:size).and_return(5)
+                allow(destroyer).to receive(:size).and_return(4)
                 player.position_ship(carrier, 0, 0)
                 expect(player.board[0][0]).to eq carrier
                 expect(player.board[0][4]).to eq carrier
                 expect(player.unpositioned_ships).to eq [destroyer]
+                player.position_ship(destroyer, 1, 0)
+                expect(player.board[1][0]).to eq destroyer
+                expect(player.board[1][3]).to eq destroyer
+                expect(player.board[1][4]).to eq nil
+                expect(player.unpositioned_ships).to be_empty
             end
         end
     end
