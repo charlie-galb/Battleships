6f4020bbdd5c9bf80812434c801e0d2434df71c8
charlie-galb <galbraith.charlie@gmail.com>
2020-12-02 15:50:41 +0000

    Add ship classes with size variables that are publicly accessible

---
+++ lib/carrier.rb
@@ -0,0 +1,6 @@
+class Carrier
+    attr_reader :size
+    def initialize
+        @size = 5
+    end
+end
---
+++ lib/destroyer.rb
@@ -0,0 +1,6 @@
+class Destroyer
+    attr_reader :size
+    def initialize
+        @size = 4
+    end
+end
