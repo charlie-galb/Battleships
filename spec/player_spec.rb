c8e3f179502132fe8b2b322729408a0458a42885
charlie-galb <galbraith.charlie@gmail.com>
2020-12-02 16:36:57 +0000

    Add extra assertion to check ship occupies multiple squares

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,8 +9,10 @@ describe Player do
         context "At the start of the game" do
             it "can place ships on the board" do
                 player = Player.new([carrier, destroyer])
+                allow(carrier).to receive(:size).and_return(5)
                 player.position_ship(carrier, 0, 0)
                 expect(player.board[0][0]).to eq carrier
+                expect(player.board[0][4]).to eq carrier
                 expect(player.unpositioned_ships).to eq [destroyer]
             end
         end
