42b2fc120629cb3b98d5d22b9116416a5c6e30f5
charlie-galb <galbraith.charlie@gmail.com>
2020-12-02 16:10:51 +0000

    Modify test to account for multiple ship classes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,18 @@
 require 'player'
 
 describe Player do
-    context "At the start of the game" do
-        it "can place ships on the board" do
-            player = Player.new;
-            player.position_carrier(0, 0)
-            expect(player.display_board[0][0]).to eq "carrier"
+
+    let(:destroyer) {double('fake destroyer')}
+    let(:carrier) {double('fake carrier')}
+
+    describe "#position_ship" do
+        context "At the start of the game" do
+            it "can place ships on the board" do
+                player = Player.new([carrier, destroyer])
+                player.position_ship(carrier, 0, 0)
+                expect(player.board[0][0]).to eq carrier
+                expect(player.unpositioned_ships).to eq [destroyer]
+            end
         end
     end
 end
