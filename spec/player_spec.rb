d99be8fa9f4ef9586c69699ca8c49d62de52bbf8
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 16:31:00 +0000

    Test for vertical placement

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,14 +11,14 @@ describe Player do
                 player = Player.new([carrier, destroyer])
                 allow(carrier).to receive(:size).and_return(5)
                 allow(destroyer).to receive(:size).and_return(4)
-                player.position_ship(carrier, 0, 0)
+                player.position_ship(carrier, "Horizontal", 0, 0)
                 expect(player.board[0][0]).to eq carrier
                 expect(player.board[0][4]).to eq carrier
                 expect(player.unpositioned_ships).to eq [destroyer]
-                player.position_ship(destroyer, 1, 0)
+                player.position_ship(destroyer, "Vertical", 1, 0)
                 expect(player.board[1][0]).to eq destroyer
-                expect(player.board[1][3]).to eq destroyer
-                expect(player.board[1][4]).to eq "O"
+                expect(player.board[3][0]).to eq destroyer
+                expect(player.board[7][0]).to eq "O"
                 expect(player.unpositioned_ships).to be_empty
             end
         end
