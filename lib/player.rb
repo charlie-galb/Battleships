01a300a45445be122d302bc4d51aa0a0fc84058e
charlie-galb <galbraith.charlie@gmail.com>
2020-12-03 16:30:40 +0000

    Player can position ships vertically or horizontally

--- lib/player.rb
+++ lib/player.rb
@@ -11,10 +11,28 @@ class Player
         @active_ships = []
     end
 
-    def position_ship(ship, y_index, x_index)
+    def position_ship(ship, orientation, y_index, x_index)
+        if orientation.downcase == "horizontal"
+           position_horizontally(ship, y_index, x_index)
+        elsif orientation.downcase == "vertical"
+           position_vertically(ship, y_index, x_index)
+        end
+        @unpositioned_ships.delete(ship)
+    end
+
+    private 
+
+    def position_horizontally(ship, y_index, x_index)
         upper_index = x_index + ship.size - 1
         @board[y_index][x_index..upper_index] = @board[y_index][x_index..upper_index].map{ |square| square = ship}
-        @unpositioned_ships.delete(ship)
+    end
+
+    def position_vertically(ship, y_index, x_index)
+        upper_index = y_index + ship.size - 1
+        squares_to_occupy = (y_index..upper_index)
+        @board =  @board.each.with_index do |row, index| 
+            row[x_index] = ship if squares_to_occupy.include?(index)
+        end
     end
 
 end
