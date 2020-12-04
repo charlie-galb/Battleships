2691a7621b3462a4f65857144025b4130c78f467
charlie-galb <galbraith.charlie@gmail.com>
2020-12-04 14:56:33 +0000

    Add #take_turn and add second player functionality

--- lib/game.rb
+++ lib/game.rb
@@ -2,29 +2,17 @@ require_relative 'player'
 
 class Game
 
-    attr_reader :player_1
+    attr_reader :player_1, :player_2
 
     def initialize
-        @player_1
-        @player_2
-    end
-    
-    def enter_number_of_players
-        puts "Please enter the number of players"
-        number_of_players = nil
-
-        until number_of_players == "1" || number_of_players == "1"
-            number_of_players = gets.chomp
-        end
-
         @player_1 = Player.new
+        @player_2 = Player.new
     end
 
-    def player_1_position_ships
-
-        until @player_1.unpositioned_ships.length == 0
+    def position_ships(player)
+        until player.unpositioned_ships.length == 0
             puts "Type the number of the ship you want to position:"
-            @player_1.unpositioned_ships.each_with_index do |ship, index|
+            player.unpositioned_ships.each_with_index do |ship, index|
                 puts "#{index}: #{ship}"
             end
             ship_index = gets.chomp
@@ -34,8 +22,26 @@ class Game
             y_index = gets.chomp
             puts "Where on the x-axis would you like to place it?"
             x_index = gets.chomp
-            ship = player_1.unpositioned_ships[ship_index.to_i]
-            @player_1.position_ship(ship, orientation, y_index.to_i, x_index.to_i)
+            ship = player.unpositioned_ships[ship_index.to_i]
+            player.position_ship(ship, orientation, y_index.to_i, x_index.to_i)
         end
+    end
+
+    def player_1_take_turn
+        puts "Player 1, prepare to fire."
+        puts "Enter the longitude you would like to strike."
+        y_index = gets.chomp
+        puts "Enter the latitude you would like to strike."
+        x_index = gets.chomp
+        @player_1.fire(@player_2, y_index.to_i, x_index.to_i)
+    end
+
+    def player_2_take_turn
+        puts "Player 1, prepare to fire."
+        puts "Enter the longitude you would like to strike."
+        y_index = gets.chomp
+        puts "Enter the latitude you would like to strike."
+        x_index = gets.chomp
+        @player_2.fire(@player_1, y_index.to_i, x_index.to_i)
     end
 end
