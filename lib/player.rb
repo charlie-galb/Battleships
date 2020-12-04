48c7b64b1c5861b697c7908b35d116d8c63afe5f
charlie-galb <galbraith.charlie@gmail.com>
2020-12-04 11:27:50 +0000

    Add guard conditions to prevent players placing ships on top of each other

--- lib/player.rb
+++ lib/player.rb
@@ -26,6 +26,7 @@ class Player
         upper_index = x_index + ship.size - 1
         raise_error_if_ship_off_board(upper_index)
         squares_to_occupy = (x_index..upper_index).to_a
+        raise_error_if_horizontal_spaces_already_occupied(squares_to_occupy, y_index)
         squares_to_occupy.each{ |square| @board[y_index][square] = ship if square < 8 }
     end
 
@@ -33,11 +34,20 @@ class Player
         upper_index = y_index + ship.size - 1
         raise_error_if_ship_off_board(upper_index)
         squares_to_occupy = (y_index..upper_index).to_a
+        raise_error_if_vertical_spaces_already_occupied?(squares_to_occupy, x_index)
         squares_to_occupy.each{ |square| @board[square][x_index] = ship if square < 8 }
     end
 
     def raise_error_if_ship_off_board(upper_index)
         return raise "Not enough space to put your ship here!" if upper_index > 7 
+    end
+
+    def raise_error_if_horizontal_spaces_already_occupied(squares_to_occupy, y_index)
+        squares_to_occupy.each{ |square| return raise "Space already occupied" if @board[y_index][square] != "O" }
+    end
+
+    def raise_error_if_vertical_spaces_already_occupied?(squares_to_occupy, x_index)
+        squares_to_occupy.each{ |square| return raise "Space already occupied" if @board[square][x_index] != "O" }
     end
 
 end
