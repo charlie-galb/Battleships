2cc9755d5a2a7244dab24ba6a08730aa437d8753
charlie-galb <galbraith.charlie@gmail.com>
2020-12-04 10:47:31 +0000

    Refactor guard condition into private method

--- lib/player.rb
+++ lib/player.rb
@@ -24,16 +24,20 @@ class Player
 
     def position_horizontally(ship, y_index, x_index)
         upper_index = x_index + ship.size - 1
-        return raise "Not enough space to put your ship here!" if upper_index > 7 
+        raise_error_if_ship_off_board(upper_index)
         squares_to_occupy = (x_index..upper_index).to_a
         squares_to_occupy.each{ |square| @board[y_index][square] = ship if square < 8 }
     end
 
     def position_vertically(ship, y_index, x_index)
         upper_index = y_index + ship.size - 1
-        return raise "Not enough space to put your ship here!" if upper_index > 7 
+        raise_error_if_ship_off_board(upper_index)
         squares_to_occupy = (y_index..upper_index).to_a
         squares_to_occupy.each{ |square| @board[square][x_index] = ship if square < 8 }
+    end
+
+    def raise_error_if_ship_off_board(upper_index)
+        return raise "Not enough space to put your ship here!" if upper_index > 7 
     end
 
 end
