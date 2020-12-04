4258fa3e9d5935764ad009e5f9c0fa01669505fa
charlie-galb <galbraith.charlie@gmail.com>
2020-12-04 14:55:01 +0000

    Add hit_count variable

--- lib/carrier.rb
+++ lib/carrier.rb
@@ -1,8 +1,9 @@
-require 'ship'
+require_relative 'ship'
 
 class Carrier < Ship
     attr_reader :size
     def initialize
+        @hit_count = 0
         @size = 5
     end
 end
--- lib/destroyer.rb
+++ lib/destroyer.rb
@@ -1,8 +1,9 @@
-require 'ship'
+require_relative 'ship'
 
 class Destroyer < Ship
     attr_reader :size
     def initialize
+        @hit_count = 0
         @size = 4
     end
 end
