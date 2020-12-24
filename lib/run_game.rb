caf7c60376cce9c41b4cf39fc0e761018b1f1e5a
charlie-galb <galbraith.charlie@gmail.com>
2020-12-24 12:35:56 +0000

    Update with new take_turn functions and add until statement to check for winner

--- lib/run_game.rb
+++ lib/run_game.rb
@@ -1,6 +1,9 @@
 require_relative 'game'
 
 game = Game.new
-game.enter_number_of_players
-game.player_1_position_ships
-print game.player_1.board
+game.position_ships(game.player_1)
+game.position_ships(game.player_2)
+until game.winner
+    game.player_1_take_turn
+    game.player_2_take_turn
+end
