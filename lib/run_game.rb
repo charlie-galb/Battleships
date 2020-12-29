8d0e7fcc9eee07027ebd197a5ba03caf8cf586f0
charlie-galb <galbraith.charlie@gmail.com>
2020-12-29 12:04:39 +0000

    Debug while loop to stop player_2 getting turn after p1 wins

--- lib/run_game.rb
+++ lib/run_game.rb
@@ -3,7 +3,7 @@ require_relative 'game'
 game = Game.new
 game.position_ships(game.player_1)
 game.position_ships(game.player_2)
-until game.winner
+while !game.winner
     game.player_1_take_turn
-    game.player_2_take_turn
+    game.player_2_take_turn if !game.winner 
 end
